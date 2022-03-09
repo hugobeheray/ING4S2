dbcb25f4f86f10f9d3b25d0afac00b503c2a7a2e
Hugo Beheray <hbeheray@gmail.com>
2022-03-09 15:24:17 +0100

    feat: lab5 c#

---
+++ C#/lab5/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PART2.1
+            // List<string> data = new List<string>();
+
+            // data.Add("ROME");
+            // data.Add("LONDON");
+            // data.Add("NAIROBI");
+            // data.Add("CALIFORNIA");
+            // data.Add("ZURICH");
+            // data.Add("AMSTERDAM");
+
+            // Console.WriteLine("Enter first letter");
+            // string firstLetter = Console.ReadLine();
+            // Console.WriteLine("Enter last letter");
+            // string lastLetter = Console.ReadLine();
+
+            // for (var i = 0; i < data.Count; i++)
+            // {
+            //     if (data[i].StartsWith(firstLetter) && data[i].EndsWith(lastLetter))
+            //     {
+            //         Console.WriteLine(data[i]);
+
+            //     }
+            // }
+
+            //PART2.2
+            // List<int> data = new List<int>();
+
+            // Console.WriteLine("Input the number of members on the List");
+            // int input = Int32.Parse(Console.ReadLine());
+
+            // for (int i = 0; i < input; i++)
+            // {
+            //     Console.WriteLine("Member " + i);
+            //     int nb = Int32.Parse(Console.ReadLine());
+            //     data.Add(nb);
+            // }
+
+            // Console.WriteLine("Input the value above you want to display the members of the List :");
+            // int nb2 = Int32.Parse(Console.ReadLine());
+
+
+            // for (var i = 0; i < data.Count; i++)
+            // {
+            //     if (nb2 < data[i])
+            //     {
+            //         Console.WriteLine(data[i]);
+
+            //     }
+            // }
+
+            //PART2.3
+            List<int> data = new List<int>();
+            data.Add(2);
+            data.Add(3);
+            data.Add(4);
+            data.Add(5);
+            data.Add(1);
+            data.Add(10);
+            data.Add(50);
+            data.Add(39);
+            data.Add(45);
+            data.Add(34);
+
+
+            System.Console.WriteLine("The members of the list are :");
+            for (var i = 0; i < data.Count; i++)
+            {
+                System.Console.WriteLine(data[i]);
+            }
+
+            System.Console.WriteLine("How many records you want to display ? ");
+            int input = Int32.Parse(Console.ReadLine());
+            data.Sort();
+
+            for (var i = 0; i < input; i++)
+            {
+
+                System.Console.WriteLine(data[i]);
+            }
+
+
+
+        }
+    }
+}
+
+
--- Machine Learning/Neural Network/pre_lab1/ffnn.py
+++ Machine Learning/Neural Network/pre_lab1/ffnn.py
@@ -7,11 +7,13 @@
 
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 N = 2   # number of features (columns)
 I = 71  # number of input (rows)
 K = 5   # number of neurons in hidden layer
-J = 3   # number of possible prediction (rows at the end)
+J = 3   # number of output neurons (possible prediction)
+alpha = 0.01
 
 # we load data and only keep features
 data = np.loadtxt("data_ffnn_3classes.txt", dtype=float)
@@ -22,7 +24,7 @@ biais = np.ones((I,1))
 X_ = np.concatenate((biais,X), axis =1)
 
 # we create V with random values for the weights
-V = np.random.randn(N+1,K) *.01
+V = np.random.rand(N+1,K) *.01
 
 # we multiply the value of the input with the weight
 X__ = np.dot(X_,V)
@@ -31,7 +33,7 @@ X__ = np.dot(X_,V)
 # FIRST LAYER :
 
 # activation : we measure how relevant each values is
-# we create and fill F : F are values between 0 and 1 thanks to the exponential function (sygmoide)
+# we create and fill F : F are values between 0 and 1 thanks to the sygmoide function
 F = np.zeros((I, K))
 for i in range (I):
     for j in range(K):
@@ -46,7 +48,7 @@ F = np.reciprocal(F)
 F_ = np.concatenate((biais,F), axis =1)
 
 # we create random weights
-W = np.random.randn(K+1, J) *.01
+W = np.random.rand(K+1, J) *.01
 
 # we create F__ by multiplying F_ and W
 F__ = F_.dot(W)
@@ -60,14 +62,39 @@ for i in range (I):
 G = np.reciprocal(G)
 
 
+# we create Y to get 1 columns with the results normalized
+Y = np.zeros((I,1))
+
+
+Y = np.zeros((71, 3))
+for i in range(71):
+    if (data[i][2] == 0):
+        Y[i][0] = 1
+    elif (data[i][2] == 1):
+        Y[i][1] = 1
+    elif (data[i][2] == 2):
+        Y[i][2] = 1
+
+
 # ERROR :
 
 E = 0
 
 for i in range (I):
     for j in range(J):
-        E = E + math.pow(data[i][N] - G[i][j], 2)
+        E = E + math.pow(Y[i][j] - G[i][j], 2)
 
 E = E*0.5
 
 print("Error : ",E)
+
+
+# PLOT :
+
+colors = [row[2] for row in data]
+
+plt.scatter([row[0] for row in data],[row[1] for row in data], c=colors)
+
+plt.xlabel('feature 1')
+plt.ylabel('feature 2')
+plt.show()
